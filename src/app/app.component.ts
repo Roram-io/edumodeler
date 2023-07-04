95a0b385e667198a4dd1847ee7519a85407415ad
Roram <rodrigo.agama@rappi.com>
2023-07-04 00:22:30 -0500

    Added real time detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,9 @@ export class AppComponent implements OnInit {
   alertNeutral = true;
   alertRemoved = false;
 
-
+  stereotypes = ["RW", "RG", "RT", "SR", "SD", "EDR", "EDP", "ES", "EB", "ETT", "ETM", "EIC", "EIH"];
+  newCount = [];
+  oldCount = [];  
 
   @ViewChild(DiagramComponent)
   diagramComponent: DiagramComponent;
@@ -55,25 +57,61 @@ export class AppComponent implements OnInit {
   }
 
   verificarXML(){
+    var newArray = []
     var resultado: String;
     this.diagramComponent.escanearEstereotipos();
-    resultado = localStorage.getItem("XMLfile")    
-    if (resultado.includes("&#60;&#60;RW&#62;&#62;")){
-    this.alertAdded = true;
-    this.alertNeutral = false;
-    this.alertRemoved = false;
-    this.alertMessageAdded = "Se ha añadido el estereotipo RW.";
+    resultado = localStorage.getItem("XMLfile")      
+    this.stereotypes.forEach( function (e) {
+    let coincidenceRegExp = "&#60;&#60;"+e+"&#62;&#62;" 
+    let nCoincidences = resultado.split(coincidenceRegExp).length - 1;  
+    for (var i=0; i<nCoincidences; i++) {
+      newArray.push(e);
     }
-    if (resultado.includes("&#60;&#60;ERROR&#62;&#62;")){
-      this.alertAdded = false;
+    })    
+    console.log(newArray);
+    this.oldCount = this.newCount;
+    this.newCount = newArray;    
+
+    var diff = this.newCount.length - this.oldCount.length;
+    console.log("Diferencia: ");
+    console.log(diff);
+    if (diff > 0){ //Added
+      let difference = this.newCount.filter(x => !this.oldCount.includes(x));
+      this.alertAdded = true;
+      this.alertRemoved = false;
       this.alertNeutral = false;
-      this.alertRemoved = true;
-      this.alertMessageAdded = "El estereotipo ERROR no existe. Esto provocará un error de compilación.";
+      this.alertMessageAdded = "Se ha agregado el siguiente Estereotipo: <<"+ difference[0] +">>" + this.getUsability(difference[0]);
     }
+    else if (diff < 0){ //Removed
+      let difference = this.oldCount.filter(x => !this.newCount.includes(x));
+      this.alertAdded = false;
+      this.alertRemoved = true;
+      this.alertNeutral = false;
+      this.alertMessageAdded = "Se ha removido el siguiente Estereotipo: <<"+ difference[0] +">>. ";
+    }
+        
     localStorage.removeItem("XMLfile")
   }
   
-
+  getUsability(stereotype: string){
+    let usabilidad = [
+      "Se recomienda su uso cuando existen muchos atributos.",
+      "Se recomienda cuando se desea agregar elementos visuales y la tarea posee pocos atributos.",
+      "Se recomienda cuando la información puede ser clasificada en grupos para mostrarse de forma ordenada.",
+      "Este estereotipo se usa cuando la información mostrada está lista para imprimir.",
+      "Este estereotipo es para cuando se deben presentar múltiples propiedades en una matriz de datos.",
+      "Es para cuando solo se puede elegir una opción de las bifurcaciones de una compuerta exclusiva.",
+      "Es para cuando pueden escogerse múltiples opciones de una bifurcación de una compuerta exclusiva.",
+      "Se recomienda cuando se navegará entre elementos o eventos diferentes en un sistema.",
+      "Este estereotipo se usa cuando se desea navegar a otra sección del sistema.",
+      "Este estereotipo se usa cuando es requerido cronometrar un evento.",
+      "Es para cuando se espera la interrupción de una actividad cronometrada y mostrar mensajes.",
+      "Este estereotipo indica una selección de un grupo de elementos amplio y variado.",
+      "Se recomienda cuando se navegará entre elementos o eventos diferentes en un sistema.",
+    ]
+   
+    return usabilidad[this.stereotypes.indexOf(stereotype)];
+  }
  
 }
 
